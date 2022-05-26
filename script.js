c038046551f470cf04f6c76cabfe09769eb6547f
thomasquigley56 <thomasquigley@my.unt.edu>
2022-05-26 09:39:36 -0500

    add hover, add size button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
-const vert = [];
-const horiz = [];
+let vert = [];
+let horiz = [];
 
 for(let i = 0; i < 16; i++){
     vert[i] = document.createElement('div');
@@ -9,7 +9,42 @@ for(let i = 0; i < 16; i++){
     for(let j = 0; j < 16; j++){
         horiz[i][j] = document.createElement('div'); 
         horiz[i][j].classList.add('horiz');
+        horiz[i][j].addEventListener('mouseover', function(){
+            horiz[i][j].classList.add("colored");
+        });
         vert[i].appendChild(horiz[i][j]);
     }
     container.appendChild(vert[i]);
+}
+
+const sizeBtn = document.querySelector('.sizeBtn');
+sizeBtn.addEventListener('click', function(){
+    let size = prompt("How many squares wide would you like your grid?");
+    clearContainer();
+    newGrid(size);
+});
+
+function newGrid(size){
+    vert = [];
+    horiz = [];
+    for(let i = 0; i < size; i++){
+        vert[i] = document.createElement('div');
+        vert[i].classList.add('vert');
+        horiz[i] = [];
+        for(let j = 0; j < size; j++){
+            horiz[i][j] = document.createElement('div'); 
+            horiz[i][j].classList.add('horiz');
+            horiz[i][j].addEventListener('mouseover', function(){
+                horiz[i][j].classList.add("colored");
+            });
+            vert[i].appendChild(horiz[i][j]);
+        }
+        container.appendChild(vert[i]);
+    }
+}
+
+function clearContainer(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
 }
